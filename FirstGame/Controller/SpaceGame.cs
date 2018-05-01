6af31210ead950c87b1b9935280ffbb92c922882
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-05-01 12:26:48 -0600

    I added a Parallaxing Background to my game

--- FirstGame/Controller/SpaceGame.cs
+++ FirstGame/Controller/SpaceGame.cs
@@ -29,6 +29,12 @@ namespace FirstGame.Controller
 		// A movement speed for the player
 		private float playerMoveSpeed;
 
+		// Image used to display the static background
+		private Texture2D mainBackground;
+
+		// Parallaxing Layers
+		private ParallaxingBackground bgLayer1;
+		private ParallaxingBackground bgLayer2;
 
 		public Game1()
 		{
@@ -49,6 +55,9 @@ namespace FirstGame.Controller
 
 			// Set a constant player move speed
 			playerMoveSpeed = 8.0f;
+
+			bgLayer1 = new ParallaxingBackground();
+			bgLayer2 = new ParallaxingBackground();
 
 			base.Initialize();
 		}
@@ -72,7 +81,11 @@ namespace FirstGame.Controller
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
 
+			// Load the parallaxing background
+			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
+			bgLayer2.Initialize(Content, "Texture/bgLayer2", GraphicsDevice.Viewport.Width, -2);
 
+			mainBackground = Content.Load<Texture2D>("Texture/mainbackground");
 		}
 
 		/// <summary>
@@ -121,6 +134,13 @@ namespace FirstGame.Controller
 			player.Draw(spriteBatch); 
 			// Stop drawing 
 			spriteBatch.End();
+
+			spriteBatch.Draw(mainBackground, Vector2.Zero, Color.White);
+
+			// Draw the moving background
+			bgLayer1.Draw(spriteBatch);
+			bgLayer2.Draw(spriteBatch);
+
 
 			base.Draw(gameTime);
 		}
