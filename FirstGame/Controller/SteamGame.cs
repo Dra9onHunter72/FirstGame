f0139bde8d806412ca5cd26b4f0ab7d882aee48f
Alan Burr <31518721+Dra9onHunter72@users.noreply.github.com>
2018-05-02 12:23:08 -0600

    I created my game and put sound to it

--- FirstGame/Controller/SteamGame.cs
+++ FirstGame/Controller/SteamGame.cs
@@ -6,6 +6,8 @@ using Microsoft.Xna.Framework.Input;
 using FirstGame.Model;
 using FirstGame.View;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Media;
 
 namespace FirstGame.Controller
 {
@@ -57,6 +59,15 @@ namespace FirstGame.Controller
 
 		private Texture2D explosionTexture;
 		private List<Animation> explosions;
+
+		// The sound that is played when a laser is fired
+		private SoundEffect laserSound;
+
+		// The sound used when the player or an enemy dies
+		private SoundEffect explosionSound;
+
+		// The music played during gameplay
+		private Song gameplayMusic;
 
 		public Game1()
 		{
@@ -133,6 +144,17 @@ namespace FirstGame.Controller
 			projectileTexture = Content.Load<Texture2D>("Texture/laser");
 
 			explosionTexture = Content.Load<Texture2D>("Animation/explosion");
+
+			// Load the music
+			gameplayMusic = Content.Load<Song>("Sound/gameMusic");
+
+			// Load the laser and explosion sound effect
+			laserSound = Content.Load<SoundEffect>("Sound/laserFire");
+			explosionSound = Content.Load<SoundEffect>("Sound/explosion");
+
+			// Start the music right away
+			PlayMusic(gameplayMusic);
+
 		}
 
 		/// <summary>
@@ -262,7 +284,12 @@ namespace FirstGame.Controller
 
 				// Add the projectile, but add it to the front and center of the player
 				AddProjectile(player.Position + new Vector2(player.Width / 2, 0));
+
+				// Play the laser sound
+				laserSound.Play();
 			}
+
+
 
 			player.Update(gameTime);
 		}
@@ -311,11 +338,15 @@ namespace FirstGame.Controller
 					{
 						// Add an explosion
 						AddExplosion(enemies[i].Position);
+
+						// Play the explosion sound
+						explosionSound.Play();
 					}
 
 					enemies.RemoveAt(i);
 				}
 			}
+
 		}
 
 		private void UpdateCollision()
@@ -411,6 +442,21 @@ namespace FirstGame.Controller
 					explosions.RemoveAt(i);
 				}
 			}
+		}
+
+		private void PlayMusic(Song song)
+		{
+			// Due to the way the MediaPlayer plays music,
+			// we have to catch the exception. Music will play when the game is not tethered
+			try
+			{
+				// Play the music
+				MediaPlayer.Play(song);
+
+				// Loop the currently playing song
+				MediaPlayer.IsRepeating = true;
+			}
+			catch { } //No Exception is handled so it is an empty/anonymous exception
 		}
 
 	}
